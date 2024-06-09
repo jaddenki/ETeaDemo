c464d79635b19fce1e2ff90f1a0fac16d0188c23
jaddenki <jaddenki@gmail.com>
2024-06-09 12:32:50 -0700

    added customer walk cycle

--- Assets/Scripts/CustomerWalk.cs
+++ Assets/Scripts/CustomerWalk.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CustomerWalk : MonoBehaviour
 {
      public SpriteRenderer anim;
+     public Sprite Clear;
+     public Sussy sussy;
 
     // customer animations
     public Sprite[] cust1;
@@ -13,11 +17,11 @@ public class CustomerWalk : MonoBehaviour
     public Sprite[] cust4;
     public Sprite[] cust5;
 
-    // interval to trigger animations
+    // time interval to trigger animations
     public float minInterval = 4.0f;
     public float maxInterval = 6.0f;
 
-    public float frameInterval = 0.2f; // 200 ms
+    public float frameInterval = .2f; // 200 ms
     private int cf; // current frame
 
         void Start()
@@ -27,15 +31,83 @@ public class CustomerWalk : MonoBehaviour
             anim = GetComponent<SpriteRenderer>();
         }
 
-        // Start the coroutine to handle random animation triggering
+        // lets wait!!! let the alien chil!!!
         StartCoroutine(WaitBeforeAnimation());
+        StartCoroutine(ChooseAnimation());
+
+
    
     }
 
     private IEnumerator WaitBeforeAnimation()
     {
-        yield return new WaitForSeconds(8f); // lets wait this long before we actually start animation 
+        yield return new WaitForSeconds(2f); // lets wait this long before we actually start animation 
     }
 
+    private IEnumerator ChooseAnimation()
+    {
+        while (true)
+        {
+            // wait for a set amount of time
+            float waitTime = Random.Range(minInterval, maxInterval);
+            yield return new WaitForSeconds(waitTime);
+
+            // randomly choose an a character animation to play..
+            int randomAnimationIndex = Random.Range(0, 5);
+            Sprite[] selectedAnimation;
+            // im about to choose which animation
+            switch (randomAnimationIndex)
+            {
+                case 0:
+                    selectedAnimation = cust1;
+                    break;
+                case 1:
+                    selectedAnimation = cust1;
+                    break;
+                case 2:
+                    selectedAnimation = cust1;
+                    break;
+                case 3:
+                    selectedAnimation = cust1;
+                    break;
+                case 4:
+                    selectedAnimation = cust1;
+                    break;
+                case 5:
+                    selectedAnimation = cust1;
+                    break;
+                default:
+                    selectedAnimation = cust1; // default ahh
+                    break;
+            }
+
+            yield return StartCoroutine(Walk(selectedAnimation));
+        
+        }
+
+    }
+    private IEnumerator Walk(Sprite[] chosenAnim)
+    {
+        Debug.Log("Starting Animation");
+        cf = 0;
+        while (cf < chosenAnim.Length)
+        {
+            Debug.Log(cf);
+            // warning that cust gonna check. this is where u wanna pput the warning sign animation
+            if((cf == 18) || (cf == 28) || (cf == 65) || (cf == 70))
+            {
+                Debug.Log("Hoh hoh imma check");
+            }
+            // cust is checking
+            if ((cf == 18) || (cf == 33) || (cf == 60) || (cf == 75))
+            {
+                sussy.IsSussy();
+            }
+        anim.sprite = chosenAnim[cf];
+        cf++;
+        yield return new WaitForSeconds(frameInterval);
+        }
+        anim.sprite = Clear;
+    }
 
 }
